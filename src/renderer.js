47e1e4b417734c2e106edd2b74b6024e9e8b7807
Leroux Benoit <Benoxiid@gmail.com>
2020-03-17 17:59:45 +0100

    fixed missing slash

--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,5 @@
 // Nami's stuff !
-import '.style/main.styl'
+import './style/main.styl'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
